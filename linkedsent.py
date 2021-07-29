15785676dc4beca8d91d694cd0989e2051b8c09f
dmytronorth <dandypst@gmail.com>
2021-07-29 01:28:28 -0700

    mardown jupyter

--- linkedsent.py
+++ linkedsent.py
@@ -1,3 +1,17 @@
+# %% [markdown]
+
+# # Linkedin jobs sentiment visualization and web scraping - `Python`, `JavaScript`
+
+# ## Description
+# This `Python` script scrapes up to 100 most recent Linkedin Job Postings of any Job Title and creates sentiment visualization in a form of a **word cloud**.
+
+# ## Setting up
+# First, we are importing all the necessary libraries. We are also specifying **Job Title** we wish to visualize and the number of **Job Postings** to scrape. We are then creating a link based on a Job Title.
+#
+# Note by default jobs' location is set to **Canada**. It can be changed to any other location simply by pasting Linkedin URL that contains desired Job Postings in the desired location.
+#
+# We are also setting up `Chrome Driver` locally, specifying its' path. And finally opening the link in a Chrome browser with `selenium`
+
 # %%
 # Importing libraries and specifying URL and Chrome driver path
 import time
@@ -8,9 +22,9 @@ from wordcloud import WordCloud
 import advertools as adv
 import matplotlib.pyplot as plt
 
-# specifying URL and nuber of job postings
+# specifying URL and number of job postings
 postings_name = 'Data Analyst'
-position_num = 100  # numbers 1 to 100
+position_num = 10  # numbers 1 to 100
 
 # building linkedin link
 posit = '%20'.join(postings_name.split())
@@ -22,6 +36,15 @@ path = '/Users/dmytrokryvonog/Documents/DAT/gda/gdasent/chromedriver'
 # opening url in Chrome browser
 driver = webdriver.Chrome(path)
 driver.get(url)
+
+# %% [markdown]
+# Now the link is opened in **Chrome browser**. However, most job postings are **hidden** and cannot be scraped. We will have to scroll the webpage down before we can scrape.
+
+#  ![linkedin-data-analyst-canada](img/linkedin-dataanalyst.png)
+
+# ## Loading webpage by scrolling down with `JavaScript`
+
+# Since `Python` does not have a built-in function to scroll pages we are using `JavaScript` to scroll the webpage down and also to check **body height** to determine when to stop the loop.
 
 # %%
 # scrolling down the webpage with javascript
@@ -42,6 +65,15 @@ while True:
     # updating previous height for the next loop
     previous_height = new_height
 
+# %% [markdown]
+# After 100 job postings there is a "See more jobs" button. Since 100 jobs should be enough for hour purposes we don't proceed with pressing the button.
+
+# ![linkedin-data-analyst-canada](img/linkedin-dataanalyst-scrolldown.png)
+
+# ## Scraping job postings' links
+
+# Once we have all 100 job postings loaded we can **scrape the links** with `selenium`.
+
 # %%
 # extracting hrefs
 # specifying a class where hrefs are located
@@ -58,9 +90,16 @@ for lnk in lnks[:position_num]:
 for y in range(3):
     print(links_list[y])
 
+# %% [markdown]
+
+# ## Parsing and scraping each job posting
+
+# Now we can use `requests.get()` to parse each link, then `BeautifulSoup` module to scrape the text of **Job Description**. All text is appended to one string.
+
 # %%
 # scraping URLs' contents and combining into one string
 # creating text string
+time.sleep(1)
 words_str = ''
 
 # try/except to avoid mistakes
@@ -84,6 +123,14 @@ except Exception as e:
 # previewing strings contents
 print(words_str[:500])
 
+# %% [markdown]
+# ## Plotting word cloud
+
+# Since Canadian Job Postings can be in **English and French** we are using **stop words** from `advertools` module by combining two languages in one set.
+#
+# We then generate `WordCloud` class and plot it using `matplotlib`, then save as `.png` in **png** folder.
+
+
 # %%
 # plotting word cloud
 # joining the sets of stopwords: English and French
@@ -97,7 +144,7 @@ wordcloud = WordCloud(width=800, height=800,
                       ).generate(words_str)
 
 # plotting the WordCloud image with matplotlib
-plt.figure(figsize=(25, 25), facecolor='black')
+plt.figure(figsize=(10, 10), facecolor='Black')
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.tight_layout(pad=0)
@@ -110,6 +157,4 @@ plt.savefig(f_path)
 # printing the plot
 plt.show()
 
-# Linkedin Canada jobs sentiment web scraping and visualization - Python
-
-# This Python script scrapes up to 100 most recent Linkedin job postings of any job title and creates sentiment visualization in a form of a word cloud.
+# %%
