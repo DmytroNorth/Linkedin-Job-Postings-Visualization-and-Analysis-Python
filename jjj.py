f023ec1066cf95de9d6b8707ad0c61a586e2a3b5
dmytronorth <dandypst@gmail.com>
2021-07-29 14:17:12 -0700

    img4

--- jjj.py
+++ jjj.py
@@ -8,6 +8,29 @@
 #
 # ![linkedin-data-analyst-canada-scrolldown](linkedin-dataanalyst-scrolldown.png)
 #
+# ![linkedin-data-analyst-canada](img/linkedin-dataanalyst.jpeg)
+#
+# ![linkedin-data-analyst-canada-scrolldown](/img/linkedin-dataanalyst-scrolldown.jpeg)
+#
+# ![linkedin-data-analyst-canada-scrolldown](/linkedin-dataanalyst-scrolldown.jpeg)
+#
+# ![linkedin-data-analyst-canada-scrolldown](linkedin-dataanalyst-scrolldown.jpeg)
+#
+#  ![linkedin-data-analyst-canada](img/linkedin-dataanalyst.png)
+#
+#   ![linkedin-data-analyst-canada-scrolldown](/img/linkedin-dataanalyst-scrolldown.png)
+#
+#  ![linkedin-data-analyst-canada-scrolldown](/linkedin-dataanalyst-scrolldown.png)
+#
+#  ![linkedin-data-analyst-canada-scrolldown](linkedin-dataanalyst-scrolldown.png)
+#
+#  ![linkedin-data-analyst-canada](img/linkedin-dataanalyst.jpeg)
+#
+#  ![linkedin-data-analyst-canada-scrolldown](/img/linkedin-dataanalyst-scrolldown.jpeg)
+#
+#  ![linkedin-data-analyst-canada-scrolldown](/linkedin-dataanalyst-scrolldown.jpeg)
+#
+#  ![linkedin-data-analyst-canada-scrolldown](linkedin-dataanalyst-scrolldown.jpeg)
 
 # %%
 print('whasup')
