88a75e257cd577533b36d4df7e48db02ecc3ef03
dmytronorth <dandypst@gmail.com>
2021-07-29 14:13:12 -0700

    img test2

--- jjj.py
+++ jjj.py
@@ -1,7 +1,12 @@
 # %% [markdown]
 #
 # ![linkedin-data-analyst-canada](img/linkedin-dataanalyst.png)
+#
 # ![linkedin-data-analyst-canada-scrolldown](/img/linkedin-dataanalyst-scrolldown.png)
+#
+# ![linkedin-data-analyst-canada-scrolldown](/linkedin-dataanalyst-scrolldown.png)
+#
+# ![linkedin-data-analyst-canada-scrolldown](linkedin-dataanalyst-scrolldown.png)
 #
 
 # %%
